0d042cad6eaa969e9262d914ccc639ae350bf15b
Michael Wu <wuxiaohua1011@berkeley.edu>
2021-12-05 16:23:03 -0800

    Using the rotation of controller for steering

--- Assets/Script/UDP_receiver_with_gui.cs
+++ Assets/Script/UDP_receiver_with_gui.cs
@@ -17,6 +17,7 @@ public class UDP_receiver_with_gui : MonoBehaviour
     public float throttle = 0;
     public float steering = 0;
     public String host = "127.0.0.1";
+    /*public String host = "10.0.0.29";*/
 
     public float MAX_FORWARD_THROTTLE = 1;
     public float MAX_REVERSE_THROTTLE = -1;
@@ -33,16 +34,15 @@ public class UDP_receiver_with_gui : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
         tex = new Texture2D(2, 2);
-        //vehicle_state_streamer = new VehicleStatemStreamer(host, 8003, 200);
-        //vehicle_state_streamer.Start();
+        vehicle_state_streamer = new VehicleStatemStreamer(host, 8003, 100);
+        vehicle_state_streamer.Start();
 
         rgb_streamer = new RGBStreamer(host, 8001, 100);
         rgb_streamer.Start();
 
-        //control_streamer = new ControlStreamer(host, 8004, 200);
-        //control_streamer.Start();
+        control_streamer = new ControlStreamer(host, 8004, 100); //significant latency when interval is lower than 500
+        control_streamer.Start();
 
     }
 
@@ -52,8 +52,8 @@ public class UDP_receiver_with_gui : MonoBehaviour
     {
         ParseOculusControl();
         UpdateImage();
-        RegularizeControl(); 
-        //control_streamer.UpdateControl(throttle, steering);
+        RegularizeControl();
+        control_streamer.UpdateControl(throttle, steering);
 
         float turning_angle = ExtensionMethods.Remap(steering, -1, 1, -160, 160);
         steeringWheel.transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, -turning_angle);
@@ -62,9 +62,9 @@ public class UDP_receiver_with_gui : MonoBehaviour
         float rpm_angle = ExtensionMethods.Remap(Math.Abs(throttle), 0, 1, 30, -205);
         indicator_rpm.transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, rpm_angle);
 
-        /*float speed = vehicle_state_streamer.vehicleState.getSpeed();
+        float speed = vehicle_state_streamer.vehicleState.getSpeed();
         float speed_angle = ExtensionMethods.Remap(Math.Abs(speed), 0, 3, 30, -205);
-        indicator_speed.transform.rotation = Quaternion.Euler(transform.eulerAngles.x, -20, speed_angle);*/
+        indicator_speed.transform.rotation = Quaternion.Euler(transform.eulerAngles.x, -20, speed_angle);
     }
 
     void RegularizeControl()
@@ -82,13 +82,23 @@ public class UDP_receiver_with_gui : MonoBehaviour
     void ParseOculusControl()
     {
         OVRInput.Update();
-
+        /*Debug.Log("rotation" + OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch) + "," + OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch).eulerAngles);*/
+        /*Debug.Log(OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch).eulerAngles[2]);*/
+        float controller_rotation_angle = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch).eulerAngles[2];
+        if (controller_rotation_angle <= 180)
+        {
+            steering = -controller_rotation_angle / 180.0f;
+        }
+        else
+        {
+            steering = -(controller_rotation_angle - 360) / 180.0f; ;
+        }
         float left_trigger = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger);
         float right_trigger = OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger);
-        float right_joystick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x;
+        float right_joystick = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).x;
 
-        throttle = left_trigger * 1 + right_trigger * -1;
-        steering = right_joystick;
+        throttle = left_trigger * -1 + right_trigger * 1;
+        /*steering = right_joystick;*/
     }
 
 
